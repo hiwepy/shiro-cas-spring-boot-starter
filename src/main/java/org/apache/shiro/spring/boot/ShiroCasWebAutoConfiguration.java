5c01893838b893dfe8bbaa0c065ee4de65025d4a
vindell <hnxyhcwdl1003@163.com>
2018-09-10 16:33:33 +0800

    修改

--- src/main/java/org/apache/shiro/spring/boot/ShiroCasWebAutoConfiguration.java
+++ src/main/java/org/apache/shiro/spring/boot/ShiroCasWebAutoConfiguration.java
@@ -1,23 +1,10 @@
 package org.apache.shiro.spring.boot;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.apache.commons.collections.MapUtils;
-import org.apache.shiro.biz.realm.PrincipalRealmListener;
-import org.apache.shiro.biz.web.filter.authc.listener.LoginListener;
-import org.apache.shiro.biz.web.filter.authc.listener.LogoutListener;
 import org.apache.shiro.mgt.SubjectFactory;
 import org.apache.shiro.spring.boot.cas.CasSubjectFactory;
 import org.apache.shiro.spring.web.config.AbstractShiroWebConfiguration;
-import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
-import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.apache.shiro.web.filter.authc.AuthenticationFilter;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -27,7 +14,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ObjectUtils;
 
 
 @Configuration
@@ -42,83 +28,6 @@ import org.springframework.util.ObjectUtils;
 public class ShiroCasWebAutoConfiguration extends AbstractShiroWebConfiguration implements ApplicationContextAware  {
 
 	private ApplicationContext applicationContext;
-	
-	@Autowired
-	private ShiroBizProperties bizProperties;
-	
-	/**
-	 * 登录监听：实现该接口可监听账号登录失败和成功的状态，从而做业务系统自己的事情，比如记录日志
-	 */
-	@Bean("loginListeners")
-	public List<LoginListener> loginListeners() {
-
-		List<LoginListener> loginListeners = new ArrayList<LoginListener>();
-		
-		Map<String, LoginListener> beansOfType = getApplicationContext().getBeansOfType(LoginListener.class);
-		if (!ObjectUtils.isEmpty(beansOfType)) {
-			Iterator<Entry<String, LoginListener>> ite = beansOfType.entrySet().iterator();
-			while (ite.hasNext()) {
-				loginListeners.add(ite.next().getValue());
-			}
-		}
-		
-		return loginListeners;
-	}
-	
-	/**
-	 * Realm 执行监听：实现该接口可监听认证失败和成功的状态，从而做业务系统自己的事情，比如记录日志
-	 */
-	@Bean("realmListeners")
-	public List<PrincipalRealmListener> realmListeners() {
-
-		List<PrincipalRealmListener> realmListeners = new ArrayList<PrincipalRealmListener>();
-		
-		Map<String, PrincipalRealmListener> beansOfType = getApplicationContext().getBeansOfType(PrincipalRealmListener.class);
-		if (!ObjectUtils.isEmpty(beansOfType)) {
-			Iterator<Entry<String, PrincipalRealmListener>> ite = beansOfType.entrySet().iterator();
-			while (ite.hasNext()) {
-				realmListeners.add(ite.next().getValue());
-			}
-		}
-		
-		return realmListeners;
-	}
-	
-	/**
-	 * 注销监听：实现该接口可监听账号注销失败和成功的状态，从而做业务系统自己的事情，比如记录日志
-	 */
-	@Bean("logoutListeners")
-	public List<LogoutListener> logoutListeners() {
-
-		List<LogoutListener> logoutListeners = new ArrayList<LogoutListener>();
-		
-		Map<String, LogoutListener> beansOfType = getApplicationContext().getBeansOfType(LogoutListener.class);
-		if (!ObjectUtils.isEmpty(beansOfType)) {
-			Iterator<Entry<String, LogoutListener>> ite = beansOfType.entrySet().iterator();
-			while (ite.hasNext()) {
-				logoutListeners.add(ite.next().getValue());
-			}
-		}
-		
-		return logoutListeners;
-	}
-	
-	/**
-	 * 责任链定义 ：定义Shiro的逻辑处理责任链
-	 */
-	@Bean
-    @Override
-	protected ShiroFilterChainDefinition shiroFilterChainDefinition() {
-		DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
-		Map<String /* pattert */, String /* Chain names */> pathDefinitions = bizProperties.getFilterChainDefinitionMap();
-		if (MapUtils.isNotEmpty(pathDefinitions)) {
-			chainDefinition.addPathDefinitions(pathDefinitions);
-			return chainDefinition;
-		}
-		chainDefinition.addPathDefinition("/logout", "logout");
-		chainDefinition.addPathDefinition("/**", "authc");
-		return chainDefinition;
-	}
 	
 	@Bean
 	@Override
